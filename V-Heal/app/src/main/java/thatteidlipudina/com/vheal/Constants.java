fca7a87c37fa9e52cc7bef4204fb40656ba2aecb
Vendetta <shivanipansare@ymail.com>
2018-10-24 11:33:18 +0530

    azure

--- V-Heal/app/src/main/java/thatteidlipudina/com/vheal/Constants.java
+++ V-Heal/app/src/main/java/thatteidlipudina/com/vheal/Constants.java
@@ -1,6 +1,6 @@
 package thatteidlipudina.com.vheal;
 
 public class Constants {
-    public static final String UPLOAD_URL = "http://vheal.000webhostapp.com/upload.php";
-    public static final String IMAGES_URL = "http://192.168.94.1/AndroidImageUpload/getImages.php";
+    public static final String UPLOAD_URL = "https://vheal12.azurewebsites.net/upload.php";
+
 }
